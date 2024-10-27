63f1712365f66ee1a299c8e2e24b58732398f985
vadymmelnychenko <melnicenkovadik@gmail.com>
2024-10-28 00:20:34 +0100

    test version-bump.yml and deploy-storybook.yml

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
