c88b60805b176154df04cbcc2184c63238afd810
vadymmelnychenko <melnicenkovadik@gmail.com>
2024-11-10 05:35:02 +0100

    final updates

--- src/utils/helpers.tsx
+++ src/utils/helpers.tsx
@@ -1,29 +1,5 @@
 import { ReactElement } from 'react';
 
-export const truncateText = (
-  value: string,
-  start: number = 2,
-  end: number = 4,
-  dots: number = 3,
-) => {
-  if (!value || value.length <= start + end) {
-    return value;
-  }
-
-  const startPart = value.substring(0, start);
-  const endPart = value.substring(value.length - end);
-  const dotsPart = '.'.repeat(dots);
-
-  return `${startPart}${dotsPart}${endPart}`;
-};
-
-export const formatValue = (value: any) => {
-  if (typeof value === 'string') {
-    return parseFloat(value).toFixed(2);
-  }
-  return value.toFixed(2);
-};
-
 export const highlightHashtags = (text: string): ReactElement => {
   if (!text || !text.includes('#')) {
     return <>{text}</>;
