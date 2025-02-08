7ec697898dc0b7440fe27544be6c9da69ab7d928
melnicenkovadik <melnicenkovadik@users.noreply.github.com>
2025-02-08 11:05:55 +0000

    deploy: 73ff7ec4cb264dd87704ad0d166bc10b59e28dec

---
+++ assets/index.stories-Cb5IZNdF.js
@@ -0,0 +1,37 @@
+import{j as s}from"./jsx-runtime-DR9Q75dM.js";import{C as u,a as l,b as p,c as h,d as w,e as _,E as C}from"./index-44dFlfcb.js";import{A as d}from"./authors-Bh-Wpt1v.js";import{E as f}from"./index-uyTelg-Z.js";import"./index-DRjF_FHU.js";import"./index-DlETWc0l.js";import"./index-BxTXOyjN.js";import"./index-Bb4qSo10.js";import"./arrow-right-BOUYNn4e.js";import"./createLucideIcon-UCnLdqZo.js";import"./index-Bbth8Pe4.js";import"./index-CZSCZb7H.js";import"./index-BDbf4cOB.js";import"./index-CUWL0Rm-.js";import"./index-CpgLm8gy.js";import"./index-BxwXBefv.js";import"./index-CY6KjWoQ.js";import"./index-DwWtoRQM.js";import"./index-qspcADvA.js";import"./index-rX-Bn4lm.js";import"./index-DvdiMlNs.js";import"./tslib.es6-BUas5LQb.js";import"./index-T3KOkZhn.js";import"./index-98DmW2iR.js";import"./x-0R6qbvLI.js";const B={id:"Molecules/Carousel",title:"Verified/Molecules/Carousel",component:u,tags:["autodocs"],argTypes:{children:{control:!1}}},e={parameters:{author:d["vadym.melnicenko"],embedDesigns:["https://www.figma.com/embed?embed_host=share&url=https://www.figma.com/file/AczaSV5JD1751SAWcZshOp/Flamingo-Design-System-(Community)?node-id=53-412"],designs:["https://www.figma.com/file/AczaSV5JD1751SAWcZshOp/Flamingo-Design-System-(Community)?node-id=53-412"]},args:{className:"w-full max-w-xs",children:s.jsxs(s.Fragment,{children:[s.jsx(l,{children:Array.from({length:5}).map((g,a)=>s.jsx(p,{},a))}),s.jsx(h,{}),s.jsx(w,{}),s.jsx(_,{})]})}},x=[{src:"./assets/test_img_card.jpg"},{src:"./assets/test_img_card.jpg"},{src:"./assets/test_img_card.jpg"},{src:"./assets/test_img_card.jpg"},{src:"./assets/test_img_card.jpg"},{src:"./assets/test_img_card.jpg"},{src:"./assets/test_img_card.jpg"},{src:"./assets/test_img_card.jpg"},{src:"./assets/test_img_card.jpg"},{src:"./assets/test_img_card.jpg"},{src:"./assets/test_img_card.jpg"},{src:"./assets/test_img_card.jpg"}],t={parameters:{author:d["vadym.melnicenko"],designs:["https://www.figma.com/embed?embed_host=share&url=https://www.figma.com/file/AczaSV5JD1751SAWcZshOp/Flamingo-Design-System-(Community)?node-id=53-412"]},args:{className:"carousel-container [&>div]:pb-6 w-full",opts:{slidesToScroll:1,align:"start"},children:s.jsx(l,{className:"carousel-content -ml-2",children:x.map((g,a)=>s.jsx(p,{className:"flex w-full items-center justify-center",children:s.jsx(C,{className:"h-60 w-60",src:"./assets/test_img_card.jpg",content:s.jsx(f,{imgClassName:"h-[800px] w-[800px]",description:"Edition II #23",name:"The Custom",src:"./assets/test_img_card.jpg"})})},a))})}};var r,m,o;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  parameters: {
+    author: AUTHORS['vadym.melnicenko'],
+    embedDesigns: ['https://www.figma.com/embed?embed_host=share&url=https://www.figma.com/file/AczaSV5JD1751SAWcZshOp/Flamingo-Design-System-(Community)?node-id=53-412'],
+    designs: ['https://www.figma.com/file/AczaSV5JD1751SAWcZshOp/Flamingo-Design-System-(Community)?node-id=53-412']
+  },
+  args: {
+    className: 'w-full max-w-xs',
+    children: <>
+        <CarouselContent>
+          {Array.from({
+          length: 5
+        }).map((_, index) => <CarouselItem key={index}></CarouselItem>)}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+        <CarouselDots />
+      </>
+  }
+}`,...(o=(m=e.parameters)==null?void 0:m.docs)==null?void 0:o.source}}};var i,n,c;t.parameters={...t.parameters,docs:{...(i=t.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  parameters: {
+    author: AUTHORS['vadym.melnicenko'],
+    designs: ['https://www.figma.com/embed?embed_host=share&url=https://www.figma.com/file/AczaSV5JD1751SAWcZshOp/Flamingo-Design-System-(Community)?node-id=53-412']
+  },
+  args: {
+    className: 'carousel-container [&>div]:pb-6 w-full',
+    opts: {
+      slidesToScroll: 1,
+      align: 'start'
+    },
+    children: <CarouselContent className="carousel-content -ml-2">
+        {items.map((_item, index) => <CarouselItem key={index} className="flex w-full items-center justify-center">
+            <ExpandableImage className="h-60 w-60" src="./assets/test_img_card.jpg" content={<ExpandedImage imgClassName="h-[800px] w-[800px]" description="Edition II #23" name="The Custom" src="./assets/test_img_card.jpg" />} />
+          </CarouselItem>)}
+      </CarouselContent>
+  }
+}`,...(c=(n=t.parameters)==null?void 0:n.docs)==null?void 0:c.source}}};const G=["Playground","DaapCarousel"];export{t as DaapCarousel,e as Playground,G as __namedExportsOrder,B as default};
